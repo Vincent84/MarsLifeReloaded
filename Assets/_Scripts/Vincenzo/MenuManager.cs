0a3220d4ff0b0e274f688510ef608abcf70c9460
Vincenzo Villani <v.villani84@alice.it>
2018-10-18 17:32:54 +0200

    Updated Start Menu

--- Assets/_Scripts/Vincenzo/MenuManager.cs
+++ Assets/_Scripts/Vincenzo/MenuManager.cs
@@ -1,11 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MenuManager : MonoBehaviour {
 
-	public void StartNewGame(string sceneName)
+    public AudioMixer audioMixer;
+
+    public void SetGeneralAudio(float value)
+    {
+        audioMixer.SetFloat("GeneralVolume", value);
+    }
+
+    public void StartNewGame(string sceneName)
     {
         GameManager.newGame = true;
 		SceneManager.LoadScene(sceneName);
@@ -13,7 +22,11 @@ public class MenuManager : MonoBehaviour {
 
 	public void ExitGame()
 	{
-		Application.Quit ();
-	}
+        #if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
 
+    }
 }
